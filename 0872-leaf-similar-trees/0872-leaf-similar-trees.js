b6ca52021086150f52384b9df146d85ba175fda1
Alice Zou <Alicehappy@users.noreply.github.com>
2024-11-29 16:46:20 -0500

    Time: 1 ms (47.25%), Space: 51.9 MB (18.76%) - LeetHub

---
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+
+var dfs = function (root, leaf) {
+    if (!root) return;
+    if (!root.left && !root.right) {
+        leaf.push(root.val);
+    } else {
+        // Pass `leaf` explicitly in recursive calls
+        dfs(root.left, leaf);
+        dfs(root.right, leaf);
+    }
+
+    return leaf;
+};
+
+var leafSimilar = function (root1, root2) {
+    let leaf1 = [];
+    let leaf2 = [];
+
+    dfs(root1, leaf1);
+    dfs(root2, leaf2);
+
+    return leaf1.join("_") === leaf2.join("_");
+};
